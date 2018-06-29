27d1512bc208bc4c232875f65b9d42a758a335b1
HubertLegec <hubert.legec@gmail.com>
2018-06-29 13:42:15 +0200

    add morgan logger to mock server

--- mock-server/app/server.ts
+++ mock-server/app/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
+import morgan from 'morgan';
 import {heroesController} from './controllers';
 
 const app: express.Application = express();
 app.use(bodyParser.json({limit: '10mb'}));
+app.use(morgan('common'));
 
 const port = process.env.PORT || 3000; // todo typing
 
